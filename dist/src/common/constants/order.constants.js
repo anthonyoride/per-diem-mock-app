22f7360053194dd1db9ec8c22ea502ddd7d70108
Oride Othuke <orideothuke@gmail.com>
2021-08-15 02:40:14 +0100

    first commit

--- dist/src/common/constants/order.constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.orderConstants = void 0;
-exports.orderConstants = {
-    CUTOFF_TIME: 72
-};
-//# sourceMappingURL=order.constants.js.map
